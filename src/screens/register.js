f7e963937ba03c541a51d4f2be71b7a8784786db
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2017-12-30 02:07:53 +0700

    create register screen

---
+++ src/screens/register.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Modal, View, Text } from 'react-native';
+import { RegisterForm1 } from '../components/register_form/form_1';
+
+export class RegisterScreen extends React.Component {
+  constructor(props) {
+    super (props);
+    this.state = {
+      stage: 1
+    }
+  }
+
+  nextStage = () => {
+    if (this.state.stage < 5) {
+      this.setState({stage: this.state.stage + 1});
+    } else {
+      // TODO: redirect to dashboard
+    }
+  }
+
+  backStage = () => {
+    if (this.state.stage > 1) {
+      this.setState({stage: this.state.stage - 1});
+    } else {
+      // TODO: redirect to signin
+    }
+  }
+
+  render () {
+    return (
+      <View>
+        <Modal
+          visible={this.state.stage == 1}
+          transparent={true}
+          onRequestClose={this.backStage}
+          animationType='slide'
+        >
+          <RegisterForm1 next={this.nextStage}/>
+        </Modal>
+        <Modal
+          visible={this.state.stage == 2}
+          transparent={true}
+          onRequestClose={this.backStage}
+          animationType='slide'
+        >
+          <Text>test</Text>
+        </Modal>
+      </View>
+    );
+  }
+}
