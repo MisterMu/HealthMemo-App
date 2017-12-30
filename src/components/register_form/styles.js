099a39dbd44904c6a29058ff74344e5ffa839b60
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2017-12-30 19:23:15 +0700

    update register form styles

--- src/components/register_form/styles.js
+++ src/components/register_form/styles.js
@@ -1,31 +1,34 @@
 import { StyleSheet } from 'react-native';
 import metrics from '../../../config/metrics';
 
+const IMG_SIZE_RATIO = 0.3;
+const IMG_BORDER_RADIUS = IMG_SIZE_RATIO / 2;
+const COMP_WIDTH_RATIO = 0.5;
+
 const styles = StyleSheet.create({
   host: {
     paddingLeft: metrics.DEVICE_WIDTH * 0.1,
-    paddingRight: metrics.DEVICE_WIDTH * 0.1,
-    // flex: 1,
-    // justifyContent: 'space-around'
+    paddingRight: metrics.DEVICE_WIDTH * 0.1
   },
   form_container: {
     marginTop: metrics.DEVICE_HEIGHT * 0.15,
-    height: metrics.DEVICE_HEIGHT * 0.5,
-    // backgroundColor: 'lightblue'
+    height: metrics.DEVICE_HEIGHT * 0.5
   },
   img_container: {
-    width: metrics.DEVICE_WIDTH * 0.3,
-    height: metrics.DEVICE_WIDTH * 0.3,
-    borderRadius: metrics.DEVICE_WIDTH * 0.15,
+    width: metrics.DEVICE_WIDTH * IMG_SIZE_RATIO,
+    height: metrics.DEVICE_WIDTH * IMG_SIZE_RATIO,
+    borderRadius: metrics.DEVICE_WIDTH * IMG_BORDER_RADIUS,
     alignSelf: 'center',
     backgroundColor: 'red'
   },
   container: {
-    width: metrics.DEVICE_WIDTH * 0.6,
-    alignSelf: 'center'
+    width: metrics.DEVICE_WIDTH * COMP_WIDTH_RATIO,
+    alignSelf: 'center',
+    marginTop: 15,
+    marginBottom: 15
   },
   btn: {
-    width: metrics.DEVICE_WIDTH * 0.6,
+    width: metrics.DEVICE_WIDTH * COM,
     alignSelf: 'center',
     marginTop: 30
   }
