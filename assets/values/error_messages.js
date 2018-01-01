cf907770051afb74c02cff58da1877017f08dac5
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-01-01 17:29:19 +0700

    add error messages file to /assets/values

---
+++ assets/values/error_messages.js
@@ -0,0 +1,9 @@
+const REQUIRE = 'This field is required';
+const PHONE = 'Please enter your phone number in the format xxx-xxxxxxx';
+const EMAIL = 'Please enter a valid e-mail address';
+
+export default {
+  REQUIRE,
+  PHONE,
+  EMAIL
+}
