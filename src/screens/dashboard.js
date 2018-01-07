1dace3b291250350b08704a3ba1b2904f6e5425f
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-01-07 22:59:58 +0700

    create DashboardScreen

---
+++ src/screens/dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class DashboardScreen extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+
+    };
+  }
+
+  render () {
+    return null;
+  }
+}
