6cb7239851625a62c28683aadc1525db397bdc10
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-01-07 23:01:58 +0700

    render dashboard or register screen by user_info in storage

--- App.js
+++ App.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import { RegisterScreen } from './src/screens/register';
+import { AsyncStorage, ActivityIndicator, View } from 'react-native';
+import { RegisterScreen, DashboardScreen } from './src/screens';
 
 export default class App extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      render: ''
+    };
+  }
+
+  componentDidMount () {
+    AsyncStorage.getItem('user_info').then((data) => {
+      if (data == null) {
+        this.setState({render: 'register'});
+      } else {
+        this.setState({render: 'dashboard'});
+      }
+    }).catch(err => console.error(err));
+  }
+
   render () {
-    return <RegisterScreen/>
+    if (this.state.render === 'register') {
+      return <RegisterScreen done={() => this.setState({render: 'dashboard'})}/>
+    } else if (this.state.render === 'dashboard') {
+      return <DashboardScreen/>;
+    } else {
+      return (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <ActivityIndicator/>
+        </View>
+      );
+    }
   }
 }
