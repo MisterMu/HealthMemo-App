82ad0cd9eb1f3b35c4fe9b1c396157a341f85c24
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-01-07 23:18:46 +0700

    change dashboard screen to content screen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AsyncStorage, ActivityIndicator, View } from 'react-native';
-import { RegisterScreen, DashboardScreen } from './src/screens';
+import { RegisterScreen, ContentScreen } from './src/screens';
 
 export default class App extends React.Component {
   constructor (props) {
@@ -15,16 +15,16 @@ export default class App extends React.Component {
       if (data == null) {
         this.setState({render: 'register'});
       } else {
-        this.setState({render: 'dashboard'});
+        this.setState({render: 'content'});
       }
     }).catch(err => console.error(err));
   }
 
   render () {
     if (this.state.render === 'register') {
-      return <RegisterScreen done={() => this.setState({render: 'dashboard'})}/>
-    } else if (this.state.render === 'dashboard') {
-      return <DashboardScreen/>;
+      return <RegisterScreen done={() => this.setState({render: 'content'})}/>
+    } else if (this.state.render === 'content') {
+      return <ContentScreen/>;
     } else {
       return (
         <View style={{flex: 1, justifyContent: 'center'}}>
