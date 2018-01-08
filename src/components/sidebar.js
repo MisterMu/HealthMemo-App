459341174a67c867ba68ef399eabd6918e2a9ddb
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-01-08 22:45:46 +0700

    complete sidebar flow

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,7 +1,115 @@
 import React from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableNativeFeedback,
+  Image
+} from 'react-native';
+
+import { getIcon } from '../../assets/icons';
+import color from '../../config/color';
+import metrics from '../../config/metrics';
 
 export class Sidebar extends React.Component {
+  constructor (props) {
+    super (props);
+  }
   render () {
-    return null;
+    return (
+      <View style={styles.host}>
+        <TouchableWithoutFeedback onPress={this.navToProfile}>
+          <View style={styles.profile}>
+            <View style={styles.img}/>
+            <View style={styles.name}>
+              <Text style={styles.name_txt}>
+                {this.props.name}
+              </Text>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+        <View style={styles.list}>
+          <ItemNav onPress={this.props.nav} route='Dashboard'/>
+          <ItemNav onPress={this.props.nav} route='Profile'/>
+          <ItemNav onPress={this.props.nav} route='Setup Device'/>
+          <ItemNav onPress={this.props.nav} route='Suggestion'/>
+          <ItemNav onPress={this.props.nav} route='Hospital Information'/>
+          <ItemNav onPress={this.props.nav} route='Emergency Contact'/>
+          <ItemNav onPress={this.props.nav} route='Setting'/>
+        </View>
+      </View>
+    );
+  }
+
+  navToProfile = () => {
+    console.log('nav to profile');
+    this.props.nav('Profile');
   }
 }
+
+class ItemNav extends React.Component {
+  render () {
+    return (
+      <TouchableNativeFeedback onPress={this.navToRoute}>
+        <View style={styles.item}>
+          <Image source={getIcon(this.props.route)} style={styles.icon}/>
+          <Text style={styles.item_name}>{this.props.route}</Text>
+        </View>
+      </TouchableNativeFeedback>
+    );
+  }
+
+  navToRoute = () => {
+    this.props.onPress(this.props.route);
+  }
+}
+
+const styles = StyleSheet.create({
+  host: {
+    flex: 1,
+    paddingTop: 24
+  },
+  profile: {
+    backgroundColor: color.APP_THEME,
+    padding: 16
+  },
+  img: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: 'red',
+    marginBottom: 16
+  },
+  name: {
+    width: '100%'
+  },
+  name_txt: {
+    width: '100%',
+    fontSize: metrics.FONT_SIZE,
+    color: 'white'
+  },
+  list: {
+    paddingTop: 8,
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0, 0, 0, 0.24)'
+  },
+  item: {
+    height: 48,
+    position: 'relative'
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    opacity: 0.54,
+    position: 'absolute',
+    left: 16,
+    top: 12
+  },
+  item_name: {
+    position: 'absolute',
+    left: 72,
+    top: 12,
+    fontSize: metrics.FONT_SIZE * 0.9
+  }
+});
