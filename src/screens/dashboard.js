91a2ea340f44e0e14106ae4648744ca5e965e7e6
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-01-08 22:46:03 +0700

    remove test code in dashboard

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -11,28 +11,8 @@ export class DashboardScreen extends React.Component {
 
   render () {
     console.log('dashboard');
-    var navigationView = (
-      <View style={{flex: 1, backgroundColor: '#fff'}}>
-        <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
-      </View>
-    );
     return (
-      // <View>
-      //   <Text> asd </Text>
-      //   <Button title='btn'/>
-      // </View>
-      <DrawerLayoutAndroid
-        ref='drawer'
-        drawerWidth={200}
-        drawerPosition={DrawerLayoutAndroid.positions.Left}
-        renderNavigationView={() => navigationView}
-      >
-        <View style={{flex: 1, alignItems: 'center'}}>
-          <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello</Text>
-          <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>World!</Text>
-          <Button title='press me' onPress={this.openSidebar}/>
-        </View>
-      </DrawerLayoutAndroid>
+      null
     );
   }
 
