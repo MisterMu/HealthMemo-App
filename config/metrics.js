36e794129630cf55538d8ddef12bc163ea5f23ec
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-01-08 22:45:16 +0700

    add sidebar width to metrics config file

--- config/metrics.js
+++ config/metrics.js
@@ -8,11 +8,13 @@ const DEVICE_WIDTH = width;
 const FONT_SIZE = DEVICE_WIDTH * 0.05;
 
 const APP_BAR_HEIGHT = 56;
+const SIDEBAR_WIDTH = (width <= 320)? (width - 56) : 320;
 
 export default {
   ANDROID_STATUSBAR,
   DEVICE_HEIGHT,
   DEVICE_WIDTH,
   FONT_SIZE,
-  APP_BAR_HEIGHT
+  APP_BAR_HEIGHT,
+  SIDEBAR_WIDTH
 }
