e5d40eeb17acfcccd2a2ec189c1646d4d5c6adab
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-02-11 16:13:57 +0700

    add CONTAINER_HEIGHT to metrics config file

--- config/metrics.js
+++ config/metrics.js
@@ -8,6 +8,7 @@ const DEVICE_WIDTH = width;
 const FONT_SIZE = DEVICE_WIDTH * 0.05;
 
 const APP_BAR_HEIGHT = 56;
+const CONTAINER_HEIGHT = DEVICE_HEIGHT - APP_BAR_HEIGHT;
 const SIDEBAR_WIDTH = (width <= 320)? (width - 56) : 320;
 
 export default {
@@ -16,5 +17,6 @@ export default {
   DEVICE_WIDTH,
   FONT_SIZE,
   APP_BAR_HEIGHT,
-  SIDEBAR_WIDTH
+  SIDEBAR_WIDTH,
+  CONTAINER_HEIGHT
 }
