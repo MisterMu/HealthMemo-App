700fd57491e277e843249efb148760f25ddba183
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-02-12 15:57:12 +0700

    create sensor value file

---
+++ assets/values/sensor.js
@@ -0,0 +1,23 @@
+const SENSOR_NAME = [
+  'Sensor 1',
+  'Sensor 2',
+  'Sensor 3'
+];
+
+const SENSOR_UNIT = [
+  'mu',
+  'mu',
+  'mu'
+]
+
+const SENSOR_COLOR = [
+  'red',
+  'blue',
+  'yellow'
+]
+
+export default {
+  SENSOR_NAME,
+  SENSOR_UNIT,
+  SENSOR_COLOR
+}
