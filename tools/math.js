92cbdf8175f3b6698258a7acad4c178d3104112c
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-02-15 00:02:09 +0700

    create findSum and findMean function in math tools file

---
+++ tools/math.js
@@ -0,0 +1,11 @@
+export function findSum (arr) {
+  let tmp = 0;
+  arr.map((item) => {
+    tmp += item;
+  });
+  return tmp;
+}
+
+export function findMean (arr) {
+  return findSum(arr) / arr.lenght; 
+}
