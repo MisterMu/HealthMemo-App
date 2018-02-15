4c837b734785310d5d63c4d91f97debfdd07b080
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-02-15 16:09:00 +0700

    uninstall react-native-image-picker module

--- android/app/src/main/java/com/healthmemo/MainApplication.java
+++ android/app/src/main/java/com/healthmemo/MainApplication.java
@@ -3,7 +3,6 @@ package com.healthmemo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
 import com.horcrux.svg.SvgPackage;
 import com.polidea.reactnativeble.BlePackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ImagePickerPackage(),
             new SvgPackage(),
             new BlePackage()
       );
