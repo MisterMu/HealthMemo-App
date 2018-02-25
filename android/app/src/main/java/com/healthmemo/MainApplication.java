56b82fde46d81ad1f0ca883ed7f0026243bc941e
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-02-25 18:30:07 +0700

    import react-native-file-picker to project

--- android/app/src/main/java/com/healthmemo/MainApplication.java
+++ android/app/src/main/java/com/healthmemo/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.filepicker.FilePickerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,7 +31,8 @@ public class MainApplication extends Application implements ReactApplication {
             new RNFSPackage(),
             new PickerPackage(),
             new SvgPackage(),
-            new BlePackage()
+            new BlePackage(),
+            new FilePickerPackage()
       );
     }
 
